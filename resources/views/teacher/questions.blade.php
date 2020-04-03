073ed854889967f4704f7ee83b83892965858dcc
mohamed frouh <mohamedfrouh96@gmail.com>
2020-04-03 18:33:32 +0200

    first commit

--- resources/views/teacher/questions.blade.php
+++ resources/views/teacher/questions.blade.php
@@ -17,37 +17,37 @@
   </div>
 @if ($question->type=='complete')
    <div class="row">
-       <input type="text" value="{{$studentexam->IsCom($question->id)}}" name="{{$studentexam->id}}{{$question->id}}" class="form-control {{$studentexam->IsComBg($question->id)}}">
+       <input type="text" disabled value="{{$studentexam->IsCom($question->id)}}" name="{{$studentexam->id}}{{$question->id}}" class="form-control {{$studentexam->IsComBg($question->id)}}">
    </div>
 @elseif($question->type=='trueorfalse')
    <div class="row">
      <div class="col-6  border-black p-2 {{$studentexam->Isbg($question->id,"option1")}}">
-        <input type="radio" class="form-control-check" value="option1" {{$studentexam->IsCheck($question->id,"option1")}} name="{{$studentexam->id}}{{$question->id}}">
+        <input type="radio" disabled class="form-control-check" value="option1" {{$studentexam->IsCheck($question->id,"option1")}} name="{{$studentexam->id}}{{$question->id}}">
         <label for="option1">{{$question->option1}}</label>
      </div>
      <div class="col-6  border-black p-2 {{$studentexam->Isbg($question->id,"option2")}}">
-        <input type="radio" class="form-control-check" value="option2" {{$studentexam->IsCheck($question->id,"option2")}} name="{{$studentexam->id}}{{$question->id}}">
+        <input type="radio" disabled class="form-control-check" value="option2" {{$studentexam->IsCheck($question->id,"option2")}} name="{{$studentexam->id}}{{$question->id}}">
         <label for="option2" >{{$question->option2}}</label>
      </div>
    </div>
 @elseif($question->type=='choiceone')
    <div class="row">
      <div class="col-6  border-black p-2  {{$studentexam->IsBg($question->id,"option1")}}">
-        <input type="radio" class="form-control-check " {{$studentexam->IsCheck($question->id,"option1")}} value="option1" name="{{$studentexam->id}}{{$question->id}}">
+        <input type="radio" disabled class="form-control-check " {{$studentexam->IsCheck($question->id,"option1")}} value="option1" name="{{$studentexam->id}}{{$question->id}}">
         <label for="option1">{{$question->option1}}</label>
      </div>
      <div class="col-6  border-black p-2 {{$studentexam->IsBg($question->id,"option2")}}">
-        <input type="radio" class="form-control-check" {{$studentexam->IsCheck($question->id,"option2")}} value="option2" name="{{$studentexam->id}}{{$question->id}}">
+        <input type="radio" disabled class="form-control-check" {{$studentexam->IsCheck($question->id,"option2")}} value="option2" name="{{$studentexam->id}}{{$question->id}}">
         <label for="option2">{{$question->option2}}</label>
      </div>
    </div>
    <div class="row">
      <div class="col-6  border-black p-2 {{$studentexam->IsBg($question->id,"option3")}}">
-        <input type="radio" class="form-control-check"{{$studentexam->IsCheck($question->id,"option3")}} value="option3" name="{{$studentexam->id}}{{$question->id}}">
+        <input type="radio" disabled class="form-control-check"{{$studentexam->IsCheck($question->id,"option3")}} value="option3" name="{{$studentexam->id}}{{$question->id}}">
         <label for="option3">{{$question->option3}}</label>
      </div>
      <div class="col-6  border-black p-2 {{$studentexam->IsBg($question->id,"option4")}}">
-        <input type="radio" class="form-control-check" {{$studentexam->IsCheck($question->id,"option4")}} value="option4" name="{{$studentexam->id}}{{$question->id}}">
+        <input type="radio" disabled class="form-control-check" {{$studentexam->IsCheck($question->id,"option4")}} value="option4" name="{{$studentexam->id}}{{$question->id}}">
         <label for="option4">{{$question->option4}}</label>
      </div>
    </div>
@@ -55,21 +55,21 @@
    <div class="row">
 
      <div class="col-6  border-black p-2 {{$studentexam->IsMulBg($question->id,"option1")}}">
-        <input type="checkbox" class="form-control-check" value="option1" {{$studentexam->IsMulCheck($question->id,"option1")}} name="{{$question->id}}[]">
+        <input type="checkbox" disabled class="form-control-check" value="option1" {{$studentexam->IsMulCheck($question->id,"option1")}} name="{{$question->id}}[]">
         <label for="option1">{{$question->option1}}</label>
      </div>
      <div class="col-6  border-black p-2 {{$studentexam->IsMulBg($question->id,"option2")}}">
-        <input type="checkbox" class="form-control-check" value="option2"  {{$studentexam->IsMulCheck($question->id,"option2")}} name="{{$question->id}}[]">
+        <input type="checkbox" disabled class="form-control-check" value="option2"  {{$studentexam->IsMulCheck($question->id,"option2")}} name="{{$question->id}}[]">
         <label for="option2">{{$question->option2}}</label>
      </div>
    </div>
    <div class="row">
      <div class="col-6  border-black p-2 {{$studentexam->IsMulBg($question->id,"option3")}}">
-        <input type="checkbox" class="form-control-check" value="option3"  {{$studentexam->IsMulCheck($question->id,"option3")}} name="{{$question->id}}[]">
+        <input type="checkbox" disabled class="form-control-check" value="option3"  {{$studentexam->IsMulCheck($question->id,"option3")}} name="{{$question->id}}[]">
         <label for="option3">{{$question->option3}}</label>
      </div>
      <div class="col-6  border-black p-2 {{$studentexam->IsMulBg($question->id,"option4")}}">
-        <input type="checkbox" class="form-control-check" value="option4"  {{$studentexam->IsMulCheck($question->id,"option4")}} name="{{$question->id}}[]">
+        <input type="checkbox" disabled class="form-control-check" value="option4"  {{$studentexam->IsMulCheck($question->id,"option4")}} name="{{$question->id}}[]">
         <label for="option4">{{$question->option4}}</label>
      </div>
    </div>
